c184e2115771b5353458ac362a0bfdd2fe2d7822
Delaney H <steam.avocado@pm.me>
2024-04-03 12:51:25 -0230

    delete buttons added.

--- src/authors/AuthorById.js
+++ src/authors/AuthorById.js
@@ -1,29 +1,52 @@
-import {Button, Table} from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Table } from "react-bootstrap";
 
 function AuthorById(props) {
+    const [deleted, setDeleted] = useState(false);
+
     const handleClick = async () => {
         let id = parseInt(document.querySelector("input[name=authorId]").value);
 
         const requestOptions = {
             method: "GET",
-            redirect: "follow"
+            redirect: "follow",
         };
 
-        fetch("http://localhost:8080/api/v1/authors/"+id, requestOptions)
+        fetch("http://localhost:8080/api/v1/authors/" + id, requestOptions)
             .then((response) => response.json())
             .then((result) => {
                 console.log(result);
                 props.setActive([result]);
             })
             .catch((error) => console.error(error));
-    }
+    };
+
+    const handleDelete = async () => {
+        let id = props.active[0].id;
+
+        const requestOptions = {
+            method: "DELETE",
+            redirect: "follow",
+        };
+
+        fetch(`http://localhost:8080/api/v1/authors/delete?author_id=${id}`, requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    setDeleted(true);
+                    props.setActive([]);
+                } else {
+                    console.error("Error deleting author");
+                }
+            })
+            .catch((error) => console.error(error));
+    };
 
     return (
         <>
-            <input type="number" name="authorId" placeholder="Author ID"/>
+            <input type="number" name="authorId" placeholder="Author ID" />
             <Button onClick={handleClick}>Search</Button>
-            {
-                props.active.length > 0 ?
+            {props.active.length > 0 ? (
+                <>
                     <Table bordered hover>
                         <thead>
                         <tr>
@@ -32,15 +55,23 @@ function AuthorById(props) {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr key={"byId-"+props.active[0].id}>
+                        <tr key={"byId-" + props.active[0].id}>
                             <td>{props.active[0].id}</td>
-                            <td>{props.active[0].firstName} {props.active[0].lastName}</td>
+                            <td>
+                                {props.active[0].firstName} {props.active[0].lastName}
+                            </td>
                         </tr>
                         </tbody>
-                    </Table> : null
-            }
+                    </Table>
+                    {!deleted && (
+                        <Button variant="danger" onClick={handleDelete}>
+                            Delete
+                        </Button>
+                    )}
+                </>
+            ) : null}
         </>
-    )
+    );
 }
 
 export default AuthorById;
--- src/books/BookByIsbn.js
+++ src/books/BookByIsbn.js
@@ -1,12 +1,15 @@
+import React, { useState } from "react";
 import { Button, Table } from "react-bootstrap";
 
 function BookByIsbn(props) {
+    const [deleted, setDeleted] = useState(false);
+
     const handleClick = async () => {
         let isbn = document.querySelector("input[name=isbn]").value;
 
         const requestOptions = {
             method: "GET",
-            redirect: "follow"
+            redirect: "follow",
         };
 
         fetch(`http://localhost:8080/api/v1/books/${isbn}`, requestOptions)
@@ -16,14 +19,34 @@ function BookByIsbn(props) {
                 props.setBookByIsbn([result]);
             })
             .catch((error) => console.error(error));
-    }
+    };
+
+    const handleDelete = async () => {
+        let isbn = props.bookByIsbn[0].isbn;
+
+        const requestOptions = {
+            method: "DELETE",
+            redirect: "follow",
+        };
+
+        fetch(`http://localhost:8080/api/v1/books/delete?isbn=${isbn}`, requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    setDeleted(true);
+                    props.setBookByIsbn([]);
+                } else {
+                    console.error("Error deleting book");
+                }
+            })
+            .catch((error) => console.error(error));
+    };
 
     return (
         <>
-            <input type="text" name="isbn" placeholder="ISBN"/>
+            <input type="text" name="isbn" placeholder="ISBN" />
             <Button onClick={handleClick}>Search</Button>
-            {
-                props.bookByIsbn.length > 0 ?
+            {props.bookByIsbn.length > 0 ? (
+                <>
                     <Table bordered hover>
                         <thead>
                         <tr>
@@ -33,20 +56,24 @@ function BookByIsbn(props) {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr key={"byIsbn-"+props.bookByIsbn[0].isbn}>
+                        <tr key={"byIsbn-" + props.bookByIsbn[0].isbn}>
                             <td>{props.bookByIsbn[0].isbn}</td>
                             <td>{props.bookByIsbn[0].title}</td>
-                            {
-                                props.bookByIsbn[0].authorList.map((author) => {
-                                    return (<p>{author.firstName} {author.lastName}</p>)
-                                })
-                            }
+                            {props.bookByIsbn[0].authorList.map((author, index) => (
+                                <td key={index}>{author.firstName} {author.lastName}</td>
+                            ))}
                         </tr>
                         </tbody>
-                    </Table> : null
-            }
+                    </Table>
+                    {!deleted && (
+                        <Button variant="danger" onClick={handleDelete}>
+                            Delete Book
+                        </Button>
+                    )}
+                </>
+            ) : null}
         </>
-    )
+    );
 }
 
 export default BookByIsbn;
